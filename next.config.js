b11ef9603969f295305ac7684d1e2294d76cb460
WhoDaresWins404 <webaccount@gmx.net>
2026-06-12 15:56:25 +0200

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  env: {
-    NEXT_PUBLIC_GRAPHQL_ENDPOINT: process.env.GRAPHQL_ENDPOINT || 'http://localhost:5000/graphql',
-  },
-};
-
-module.exports = nextConfig;
